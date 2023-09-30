8513f35a6e18c4cfc7afb14f2f43e622ef592ccf
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-09-30 08:43:34 +0100

    update

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,27 +2,27 @@
 import React, { useEffect, useRef, useState } from "react";
 
 export const Hero = () => {
-  // Create a ref for the section you want to scroll to
-  const sectionRef = useRef(null);
+  // // Create a ref for the section you want to scroll to
+  // const sectionRef = useRef(null);
 
-  useEffect(() => {
-    // Use setTimeout to scroll to the section after 5 seconds
-    const scrollTimeout = setTimeout(() => {
-      // Check if the section ref is defined
-      if (sectionRef.current) {
-        // Scroll to the section using scrollIntoView
-        sectionRef.current.scrollIntoView({ behavior: "smooth" });
-      }
-    }, 3000); // 5000 milliseconds (5 seconds)
+  // useEffect(() => {
+  //   // Use setTimeout to scroll to the section after 5 seconds
+  //   const scrollTimeout = setTimeout(() => {
+  //     // Check if the section ref is defined
+  //     if (sectionRef.current) {
+  //       // Scroll to the section using scrollIntoView
+  //       sectionRef.current.scrollIntoView({ behavior: "smooth" });
+  //     }
+  //   }, 3000); // 5000 milliseconds (5 seconds)
 
-    // Cleanup the timeout if the component unmounts
-    return () => {
-      clearTimeout(scrollTimeout);
-    };
-  }, []);
+  //   // Cleanup the timeout if the component unmounts
+  //   return () => {
+  //     clearTimeout(scrollTimeout);
+  //   };
+  // }, []);
   return (
     <section
-      ref={sectionRef}
+      // ref={sectionRef}
       className="h-screen p-2 bg-black text-black "
       data-scroll-section
     >
