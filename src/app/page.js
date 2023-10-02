f4413947b25d0388f76be5c940067e5c7c957176
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-10-02 15:38:03 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -7,21 +7,21 @@ function Home() {
   return (
     <div className="w-full h-screen bg-black flex flex-col lg:flex-row relative text-black">
       <AnimatedCursor />
-      <div className=" flex lg:w-[40%] h-[45%] w-full gap-4 bg-white  flex-col justify-between   relative lg:h-full">
-        <header className="lg:p-8 p-4 ">
-          <h1 className=" lg:text-[2.2rem] text-[1.5rem] font-space flex gap-2 items-center font-[700]">
-            <div className="w-[2rem] h-[2rem] lg:w-[3rem] lg:h-[3rem]  relative">
+      <div className=" flex lg:w-[40%]  w-full gap-4 bg-white  flex-col justify-between   relative lg:h-full">
+        <header className=" p-4 ">
+          <h1 className=" lg:text-[2rem] text-[1.5rem] font-space flex gap-2 items-center font-[700]">
+            <div className="w-[2rem] h-[2rem] lg:w-[2.5rem] lg:h-[2.5rem]  relative">
               <Image src="/panda.svg" alt="panda emoji" fill priority />
             </div>
 
             <Typewriter text="dev_panda" speed={80} delay={0} />
           </h1>
-          <h1 className="lg:text-[1.5rem] text-[1.2rem] font-space flex gap-2 items-center">
+          <h1 className="lg:text-[1.2rem] text-[1rem] font-space flex gap-2 items-center">
             <Scrambler text="creative web developer" />
           </h1>
         </header>
-        <div className="w-full flex-1 flex justify-end">
-          <div className="w-[400px]  bg-black overflow-hidden relative  ">
+        <div className="w-full hidden lg:flex bg-gradient-to-r from-transparent from-5% to-black to-70% justify-end">
+          <div className=" lg:w-[370px] lg:h-[350px]  overflow-hidden relative  ">
             <Image
               src="/selfai.png"
               alt="panda emoji"
@@ -31,10 +31,10 @@ function Home() {
             />
           </div>
         </div>
-        <nav className="lg:p-8 p-4 flex lg:flex-col lg:gap-12 gap-4 font-space justify-end lg:items-end">
-          <a>about</a>
-          <a>projects</a>
-          <a>contact</a>
+        <nav className="p-4 flex lg:flex-col lg:gap-8 gap-4 font-space justify-end lg:items-end">
+          <a>SouthsideFoods</a>
+          <a>Greeth</a>
+          <a>getLinked.ai</a>
         </nav>
       </div>
 
